4a40200ff40632c7cf640fdfaa7d8c7c69f42c6c
honeyai <contact.jessicasanchez@gmail.com>
2022-06-22 17:09:21 -0400

    added delete functionality

--- Routes/routes.js
+++ Routes/routes.js
@@ -13,8 +13,6 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   console.info(`${req.method} request received to add a review`);
   const { title, text } = req.body;
-  console.log(title, text);
-
   const newNote = {
     id: uuidv4(),
     title,
@@ -44,5 +42,31 @@ router.post('/', (req, res) => {
   } else {
     res.status(500).json('Error in posting new note');
   }
+});
+
+router.delete(`/:id`, (req, res) => {
+  console.info(`${req.method} request received to delete a review`);
+
+  const exists = notes.some(note => note.id === req.params.id);
+  console.log("exists", exists);
+  if (exists) {
+    let toDelete = notes.filter(elem => elem.id !== req.params.id);
+    notes.pop(toDelete);
+    console.log("notes", notes);
+    fs.writeFile(path.dirname('../db/db.json'), JSON.stringify(notes, null, " "),
+      (err) => {
+        if (err)
+          console.log(err);
+        else {
+          console.log("File written successfully\n");
+          console.log("The written has the following contents:");
+          console.log(fs.readFileSync(path.dirname('../db/db.json'), "utf8"));
+        }
+      });
+    res.json(notes);
+  } else {
+    res.status("400").json({ message: `${req.params.id} not found.` });
+  }
 })
+
 module.exports = router;
